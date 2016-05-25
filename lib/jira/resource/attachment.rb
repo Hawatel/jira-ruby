1ae507d64a9c4dbf71b00d6cccfbad4b0fd26034
Daniel Iwaniuk <daniel.iwaniuk@hawatel.com>
2016-05-25 16:35:35 +0200

    Added attachment method for JIRA::Base

--- lib/jira/resource/attachment.rb
+++ lib/jira/resource/attachment.rb
@@ -1,12 +1,38 @@
 module JIRA
-  module Resource
+       module Resource
 
-    class AttachmentFactory < JIRA::BaseFactory # :nodoc:
+               class Attachment < JIRA::Base
+                       belongs_to :issue
+
+                       def self.endpoint_name
+                               return "attachments"
+                       end
+
+                       def save!(attrs)
+                               http_method = new_record? ? :post : :put
+                               headers = {
+                                       "X-Atlassian-Token" => "nocheck"
+                               }
+
+                               request = Net::HTTP::Post::Multipart.new url,
+                               { "file" => UploadIO.new(File.new(attrs['file']), "application/binary", attrs['file']) },
+                               headers
+                               request_client = client.request_client
+                               options = client.request_client.options
+
+                               request.basic_auth(options[:username], options[:password])
+                               response = request_client.basic_auth_http_conn.request(request)
+
+                               set_attrs(attrs, false)
+                               unless response.body.nil? or response.body.length < 2
+                                 json = self.class.parse_json(response.body)
+                                 attachment = json[0]
+                                 set_attrs(attachment)
+                               end
+
+                               @expanded = false
+                               true
+                       end
+               end
     end
-
-    class Attachment < JIRA::Base
-      has_one :author, :class => JIRA::Resource::User
-    end
-
-  end
 end
